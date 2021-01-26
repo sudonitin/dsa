92139802830457e99aee39eba3a883e179536c4d
globefire <sahun5774@gmail.com>
2021-01-26 21:52:43 +0530

    Connect Level Order Siblings (medium) -- referred efficient logic

--- educative.io/BFS/Connect Level Order Siblings (medium).py
+++ educative.io/BFS/Connect Level Order Siblings (medium).py
@@ -5,6 +5,21 @@ class TreeNode:
         self.val = val
         self.left, self.right = None, None
         self.next = None
+    # level order traversal using next pointer
+    def print_level_order(self):
+        nextLevelRoot = self
+        while nextLevelRoot:
+            current = nextLevelRoot
+            nextLevelRoot = None
+            while current:
+                print(str(current.val) + " ", end='')
+                if not nextLevelRoot:
+                    if current.left:
+                        nextLevelRoot = current.left
+                    elif current.right:
+                        nextLevelRoot = current.right
+                current = current.next
+            print()
 
 def ownLogic(root):
     queue = deque()
@@ -67,15 +82,33 @@ def ownLogic(root):
     #     print(levelStr)
     return
 
+def referredLogic(root):
+    queue = deque()
+    queue.append(root)
+    while queue:
+        previousNode = None
+        levelSize = len(queue)
+        for _ in range(levelSize):
+            currentNode = queue.popleft()
+            if previousNode:
+                previousNode.next = currentNode
+            previousNode = currentNode
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
 
 def main():
-    root = TreeNode(1)
-    root.left = TreeNode(2)
-    root.right = TreeNode(3)
-    # root.left.left = TreeNode(4)
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
     # root.left.right = TreeNode(5)
-    root.right.left = TreeNode(6)
-    root.right.right = TreeNode(7)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
     # print(ownLogic(root))
-    ownLogic(root)
+    # ownLogic(root)
+    referredLogic(root)
+    print("SOlution is")
+    root.print_level_order()
 main()
