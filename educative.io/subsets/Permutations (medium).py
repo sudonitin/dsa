def554067dc8e03ecacda380c3054af5f357f3f6
globefire <sahun5774@gmail.com>
2021-02-04 07:24:49 +0530

    Permutations --ownLogic(recursion)

---
+++ educative.io/subsets/Permutations (medium).py
@@ -0,0 +1,17 @@
+# a[:i+1] + [newElem] + a[i+1:]
+def getPermuatationOfSet(result, givenSet):
+    if len(result[0]) == 0:
+        return getPermuatationOfSet([[givenSet[0]]], givenSet)
+    else:
+        if len(result[0]) == len(givenSet):
+            # print(result)
+            return result
+        newResult = []
+        newElem = givenSet[len(result[0])]
+        for i in range(len(result)):
+            for j in range(-1, len(result[i])):
+                newResult.append(result[i][:j+1] + [newElem] + result[i][j+1:])
+        # print(newResult, "#")
+        return getPermuatationOfSet(newResult, givenSet)
+
+print(getPermuatationOfSet([[]], [1,2,3]))
