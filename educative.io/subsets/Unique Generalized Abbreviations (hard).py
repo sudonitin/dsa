175fa7e8b7d56ddad81973e967614c428a5276cd
globefire <sahun5774@gmail.com>
2021-02-06 17:06:19 +0530

    Unique Generalized Abbreviations (hard)

--- educative.io/subsets/Unique Generalized Abbreviations (hard).py
+++ educative.io/subsets/Unique Generalized Abbreviations (hard).py
@@ -1,5 +1,64 @@
+from copy import deepcopy
 
-print(find_all_subsets("abc"))
+# TOOK A HINT (EXTENSION OF PREV QUESTION i.e. Balanced Paranthesis)
+# 
+def generate_abbreviations(string):
+    result = [""]
+    list_str = list(string)
+    while True:
+        result_len = len(result)
+        new_char = list_str[len(result[0])]
+        new_result = []
+        for i in range(result_len):
+            # push char
+            new_result.append(result[i]+new_char)
+            # push 1
+            if len(result[i]) != 0 and result[i][len(result[i])-1].isdigit():
+                temp = list(result[i])
+                temp[len(result[i])-1] = str(int(temp[len(result[i])-1])+1)
+                result[i] = "".join(temp)
+                new_result.append(result[i])
+            else:
+                new_result.append(result[i]+"1")
+        result = new_result
+        if len(result[0]) == len(string):
+            return result
+
+print(generate_abbreviations("BAT"))
+
+# 2ND FAILED ATTEMPT
+# def find_all_subsets(arr):
+#     arr = list(arr)
+#     # print(arr, "%")
+#     result = [[]]
+#     for i in arr:
+#         temp = []
+#         result_len = len(result)
+#         for subset in range(result_len):
+#             # print(result[subset] + [i])
+#             temp.append(result[subset] + [i])
+#         result += temp
+#     result = ["".join(sub) for sub in result]
+#     return generate_abbreviations(result, arr)
+
+# def generate_abbreviations(result, og_str):
+#     og_str = "".join(og_str)
+#     # print(og_str, "$")
+#     # print(result)
+#     new_result = []
+#     for subset in result:
+#         if subset == "":
+#             subset = og_str
+#             # print(subset, "---")
+#         else:
+#             temp = deepcopy(og_str)
+#             subset = temp.replace(subset, str(len(subset)))
+#         # print(subset)
+#         new_result.append(subset)
+#     # print(result, "%")
+#     return set(new_result)
+
+# print(find_all_subsets("BAT"))
 
 # HAGGA HUA CODE N LOGIC
 # def find_all_subsets(arr):
