da58a31f7ce51ebb69d2fb1195b8bdef11a4c4fd
globefire <sahun5774@gmail.com>
2021-02-13 12:18:22 +0530

    kth smallest further studies

--- educative.io/Pattern Top _K_ Elements/Kth Smallest Number (easy).py
+++ educative.io/Pattern Top _K_ Elements/Kth Smallest Number (easy).py
@@ -4,6 +4,13 @@ from heapq import *
 # APPROACH 2 :- Use MaxHeap Data Structure TC = O(NLogk) SC = O(Logk)
 # https://www.geeksforgeeks.org/heap-data-structure/
 
+# We can't use minheap of size k in this because it will return the smallest number in the given array
+# and we want the kth smallest. Hence use maxheap with negative of og numbers
+
+# THE BELOW APPROACH IS NOT ALWAYS FULL PROOF so don't refer
+# We can use minheap of size n(array size) and simply return k-1th element
+# This will return kth smallest element
+
 def find_kth_smallest(arr, k):
     maxheap = []
     for i in range(k):
@@ -16,6 +23,22 @@ def find_kth_smallest(arr, k):
     # print(list(maxheap))
     return -maxheap[0]
 
-print(find_kth_smallest([1, 5, 12, 2, 11, 5], 3))
-print(find_kth_smallest([1, 5, 12, 2, 11, 5], 4))
-print(find_kth_smallest([5, 12, 11, -1, 12], 3))
+# WRONG SOLUTION FOR [3,2,3,1,2,4,5,5,6], 3 AND [3,2,1,5,6,4], 2
+# def minHeap(arr, k):
+#     minheap = []
+#     for i in arr:
+#         heappush(minheap, i)
+#     print(list(minheap))
+#     return minheap[k-1]
+
+# print(minHeap([1, 5, 12, 2, 11, 5], 3)) #5
+# print(minHeap([1, 5, 12, 2, 11, 5], 4)) #5
+# print(minHeap([5, 12, 11, -1, 12], 3)) #11
+# print(minHeap([3,2,3,1,2,4,5,5,6], 3))
+# print(minHeap([3,2,1,5,6,4], 2))
+
+print(find_kth_smallest([1, 5, 12, 2, 11, 5], 3)) #5
+print(find_kth_smallest([1, 5, 12, 2, 11, 5], 4)) #5
+print(find_kth_smallest([5, 12, 11, -1, 12], 3)) #11
+print(find_kth_smallest([3,2,3,1,2,4,5,5,6], 3))
+print(find_kth_smallest([3,2,1,5,6,4], 2))
