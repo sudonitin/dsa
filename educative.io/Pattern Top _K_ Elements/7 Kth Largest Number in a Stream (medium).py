cd8a59cce69a272e31d8c1f7e32e834ab0911611
globefire <sahun5774@gmail.com>
2021-02-24 08:53:18 +0530

    Kth Largest Number in a Stream (medium) --own

---
+++ educative.io/Pattern Top _K_ Elements/7 Kth Largest Number in a Stream (medium).py
@@ -0,0 +1,25 @@
+# Own logic
+
+from heapq import *
+
+class kthLargestInStream:
+    def __init__(self, arr, k):
+        minheap = []
+        for i in arr:
+            heappush(minheap, i)
+            if len(minheap)  > k:
+                heappop(minheap)
+        self.minheap = minheap
+
+    # update minheap
+    def add(self, num):
+        if num > self.minheap[0]:
+            heappop(self.minheap)
+            heappush(self.minheap, num)
+        return self.minheap[0]
+
+kthLargest = kthLargestInStream([3,1,5,12,2,11], 4)
+print(kthLargest.add(6)) #5
+print(kthLargest.add(13)) #6
+print(kthLargest.add(4)) #6
+print(kthLargest.add(14)) #6
